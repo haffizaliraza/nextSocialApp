32297d56fb79159d636efc425b6f9c7a7981c8ab
Ron Buchanan <ronb@Rons-iMac.fritz.box>
2024-06-20 16:50:24 +0200

    removed image content type from videos route

--- app/api/videos/upload/route.js
+++ app/api/videos/upload/route.js
@@ -18,7 +18,7 @@ const POST = async (request) => {
         // Otherwise, you're allowing anonymous uploads.
 
         return {
-          allowedContentTypes: ["video/mp4", "image/*"],
+          allowedContentTypes: ["video/mp4"],
           tokenPayload: JSON.stringify({
             // optional, sent to your server on upload completion
             // you could pass a user id from auth, or a value from clientPayload
